1a94f9f7a5fc263352a1a5f80ad2d0de6c59f14a
cathymnguyen3 <59972390+cathymnguyen3@users.noreply.github.com>
2020-11-14 23:20:17 -0500

    adding matches.php functionality

---
+++ templates/matches.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Dog_Who</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="../css/main.css">  
+<?php require('connectdb.php'); ?> 
+</head>
+
+<?php
+ $results = match_potentialadopter_dog();
+    function match_potentialadopter_dog(){
+        echo "List of interested potential adopters for each dog in the shelter:  ";
+        global $db;
+        $query = "SELECT  potential_adopter.first_name, potential_adopter.last_name, dog.name, potential_adopter.email FROM potential_adopter JOIN interested_in ON potential_adopter.AdopterID = interested_in.AdopterID JOIN dog ON dog.DogID = interested_in.DogID;";  
+        $statement = $db ->prepare($query);
+        $statement->execute();
+        $results = $statement->fetchAll();
+        $statement->closeCursor();
+        return $results; //********** dog shelter name
+    }
+?>
+
+<body>
+<br>
+ <?php foreach($results as $item): ?>
+    <tr>
+        <br>
+        <td><?php echo $item['name']; // name of dog ?></td>
+        <br>
+        <td><?php echo $item['first_name']; //name & email of potential adopter who stated interest in dog ?></td> 
+        <?php echo " " ?>
+        <td><?php echo $item['last_name']; ?></td> 
+        <br>
+        <td><?php echo $item['email']; ?></td> 
+        <br>
+    </tr>
+ <?php endforeach; ?>
+</body>
+
+
+
+
+</html>
